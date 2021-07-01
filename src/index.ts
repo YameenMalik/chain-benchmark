fb0f4ada3d714ac08e5223b306994c1ba98be3ea
YameenMalik <yameen@seedcode.io>
2021-07-02 00:42:28 +0500

    init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-//import { DETToken__factory } from "../../artifacts/typechain/factories/DETToken__factory";
 import { ethers, Wallet } from "ethers";
 import { toNumber } from './helpers'; 
 import * as fs from "fs";
+import { TASK } from './benchmark';
 const yargs = require('yargs')
 
 // ----------------------------------------------------------------//
@@ -15,28 +15,40 @@ const argv = yargs.options({
       demandOption: false,
       description: 'number of transactions to execute'
     },
-    funding: {
+    fundingAmount: {
         alias: 'f',
         demandOption: false,
         description: 'funding amount to be sent to users'
-      },    
+      },
+    rpcUrl: {
+        alias: 'u',
+        demandOption: false,
+        description: 'RPC URL for the provider'
+      },
+      walletsPass: {
+        alias: 'p',
+        demandOption: false,
+        description: 'Password to encrypt wallet'
+      },
+    gasFaucet:{
+      alias: 'g',
+      demandOption: false,
+      description: 'private key of the faucet responsible for providing eth/edg/aUSD for paying gas fee amount to wallets'
+    }
+    
 }).argv;
 
 const numTransactions:number = argv.transactions? Number(argv.transactions): 1; 
 const fundingAmount: string = argv.funding? argv.funding: "0.0001";
+const rpcUrl:string = argv.rpcUrl? argv.rpcUrl: "http://127.0.0.1:9000"; 
+const walletsPass:string = argv.walletsPass? argv.walletsPass: "Hello-Hello-Khaalo-Jello"; 
+const faucetKey:string = argv.gasFaucet? argv.gasFaucet:"31eaf7d2584b37409065bb99ec421197bb3ffc90cd900536726a0d6a15f91d80"; 
 
-const faucetKey ="31eaf7d2584b37409065bb99ec421197bb3ffc90cd900536726a0d6a15f91d80";
-const rpcUrl = "http://127.0.0.1:9000";
 const walletsPath = `${__dirname}/wallets`;
-const walletsPass = "Hello-Hello-Khaalo-Jello";
-const detAddr: string = "0x99A53a4eBaBB340Fb3fc3804b5fd81714DF88Dcb";
-const aliceAddr: string = "0x931d6514C8d5522C7489aad7aCCFfC20E6A92E57";
 
 
 const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
-const wallet = new Wallet(faucetKey, provider);
-// const detFactory = new DETToken__factory(wallet);
-// const DET = detFactory.attach(detAddr);
+const walletFaucet = new Wallet(faucetKey, provider);
 
 
 async function createWallets(numWallets: number): Promise<ethers.Wallet[]> {
@@ -47,7 +59,7 @@ async function createWallets(numWallets: number): Promise<ethers.Wallet[]> {
       to: newWallet.address,
       value: ethers.utils.parseEther(fundingAmount),
     };
-    wallets.push(newWallet);
+    wallets.push(await newWallet.connect(provider));
   }
 return wallets;
 }
@@ -65,21 +77,28 @@ async function saveWallets(wallets: Wallet[]) {
   }
 }
 
+async function initWalletFromFile(walletPath:string): Promise<Wallet>{
+  const walletData = fs.readFileSync(walletPath) as any;
+  let wallet = await Wallet.fromEncryptedJson(walletData, walletsPass);
+  wallet = await wallet.connect(provider);
+  return wallet;
+}
+
 async function loadWallets(): Promise<Wallet[]> {
   let numWallets: number = fs.existsSync(walletsPath)
     ? fs.readdirSync(walletsPath).length
     : 0;
-  const wallets: Wallet[] = [];
+  const wait = [];
   if (numWallets > 0) {
     const fileNames = await fs.readdirSync(walletsPath);
     let itr = 0;
     while(itr < Math.min(fileNames.length,numTransactions)) {
       const filePath = walletsPath + "/" + fileNames[itr];
-      const walletData = fs.readFileSync(filePath) as any;
-      wallets.push(await Wallet.fromEncryptedJson(walletData, walletsPass));
+      wait.push(initWalletFromFile(filePath));
       itr++;
     }
   }
+  const wallets: Wallet[] = await Promise.all(wait);
   return wallets;
 }
 
@@ -90,24 +109,28 @@ async function validateFunding(wallet: Wallet): Promise<boolean>{
 
 async function fundWallets(wallets: Wallet[]){
     for (let i = 0; i < numTransactions; i++) {
-        if (!await validateFunding(wallets[i])){
-            console.log("-->", i+1, "- Funding wallet:", wallets[i].address);
-            const tx = {
-            to: wallets[i].address,
-            value: ethers.utils.parseEther(fundingAmount),
-            };
-            const send = await wallet.sendTransaction(tx)
-            await send.wait();
-        }
+      const _wallet = wallets[i];
+
+      if (await validateFunding(_wallet)) {
+        continue;
       }
+
+      console.log("-->", i+1, "- Funding wallet:", _wallet.address);
+      const tx = {
+        to: _wallet.address,
+        value: ethers.utils.parseEther(fundingAmount),
+        };
+      const send = await walletFaucet.sendTransaction(tx)
+      await send.wait();
+    }
 }
 
 async function batchTransaction(wallets: Wallet[]){
-    // const waits = []
-    // for(let itr in wallets){
-    //     waits.push(DET.connect(wallets[itr]).transfer(aliceAddr,toFullDigitStr(100), {'gasLimit':2000000}));
-    // }
-    // Promise.all(waits);
+    const waits = []
+    for(let itr in wallets){
+        waits.push(TASK(wallets[itr]));
+    }
+    await Promise.all(waits);
 } 
 
 async function main(){
@@ -130,17 +153,21 @@ async function main(){
 
     wallets.push(...newWallets);
   }
-
-  wallets.map(w => w.connect(provider));
-
+  
   console.log("-> Funding wallets...")
   await fundWallets(wallets);
 
-//   console.log("-> Executing batch transactions...")
-//   var start = process.hrtime()
-//   await batchTransaction(wallets);
-//   var end = process.hrtime(start)
-//   console.info('Execution time (hr): %ds %dms', end[0], end[1] / 1000000)
+  console.log("-> Executing batch transactions...")
+  // start time
+  var start = process.hrtime()
+
+  // call task in batch
+  await batchTransaction(wallets);
+
+  // stop time
+  var end = process.hrtime(start)
+
+  console.info('Execution time (hr): %ds %dms', end[0], end[1] / 1000000)
 };
 
 
